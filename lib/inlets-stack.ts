0ed12d87af4fe71a33df861d7c43e1cb1e0e0adb
Nathan Peck <peckn@amazon.com>
2021-08-24 23:43:57 +0000

    saving progress on the demo app

--- lib/inlets-stack.ts
+++ lib/inlets-stack.ts
@@ -2,12 +2,11 @@ import * as cdk from '@aws-cdk/core';
 import * as ec2 from '@aws-cdk/aws-ec2';
 import * as ecs from '@aws-cdk/aws-ecs';
 import * as ecr from '@aws-cdk/aws-ecr';
-import * as ecrAssets from '@aws-cdk/aws-ecr-assets';
 import * as secretsManager from '@aws-cdk/aws-secretsmanager';
 import * as elbv2 from '@aws-cdk/aws-elasticloadbalancingv2';
 
 import * as path from 'path';
-import { spawnSync, SpawnSyncOptions } from 'child_process';
+import { spawnSync } from 'child_process';
 
 export class InletsStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
@@ -18,8 +17,8 @@ export class InletsStack extends cdk.Stack {
 
     const cluster = new ecs.Cluster(this, 'cluster', { vpc });
 
-    // Generate a token that will be used by clients to connect
-    // to the exit server.
+    // Generate a secret token that will be used by Inlets clients to connect
+    // to the Inlets exit server.
     var inletsToken = new secretsManager.Secret(this, 'exit-server-token', {
       generateSecretString: {
         passwordLength: 32,
@@ -31,22 +30,39 @@ export class InletsStack extends cdk.Stack {
     // Then substitute in your own secret's ARN here.
     var licenseKey = secretsManager.Secret.fromSecretCompleteArn(this, 'license-key', 'arn:aws:secretsmanager:us-east-2:209640446841:secret:inlets-license-key-UVnDul');
 
-    // Launch the Inlets exit server as a daemon in Fargate.
+    // Launch the Inlets exit server as a service in Fargate.
     var exitServerDefinition = new ecs.FargateTaskDefinition(this, 'inlets-exit-server-task', {
-      cpu: 1024,
-      memoryLimitMiB: 2048,
+      cpu: 2048,
+      memoryLimitMiB: 4096,
     });
 
-    exitServerDefinition.addContainer('inlets', {
+    var exitServerContainer = exitServerDefinition.addContainer('inlets', {
       image: ecs.ContainerImage.fromRegistry('ghcr.io/inlets/inlets-pro:0.9.0-rc2'),
       logging: new ecs.AwsLogDriver({ streamPrefix: 'inlets-exit-server' }),
-      command: ["http", "server", "--auto-tls=false", "--token-env=INLETS_TOKEN"],
+      command: ["http", "server", "--auto-tls=false", "--token-env=INLETS_TOKEN", "--port=8000"],
       secrets: {
         INLETS_TOKEN: ecs.Secret.fromSecretsManager(inletsToken),
       },
       portMappings: [
-        { containerPort: 8123, },
+        // This port is used for Inlets clients on each Raspberry Pi to
+        // connect back to the exit server and make themselves available for traffic.
+        {
+          containerPort: 8123,
+          hostPort: 8123
+        },
+        // This port is used for end users to send a request to the exit server.
+        // The request then gets sent back down to one of the connected Inlets clients.
+        {
+          containerPort: 8000,
+          hostPort: 8000
+        }
       ],
+    });
+
+    exitServerContainer.addUlimits({
+      softLimit: 1024000,
+      hardLimit: 1024000,
+      name: ecs.UlimitName.NOFILE
     });
 
     var service = new ecs.FargateService(this, 'inlets-exit-server', {
@@ -54,43 +70,62 @@ export class InletsStack extends cdk.Stack {
       taskDefinition: exitServerDefinition,
       assignPublicIp: true,
       healthCheckGracePeriod: cdk.Duration.seconds(2147483647), // Effectively infinite grace period
-      desiredCount: 1
+      desiredCount: 2 // You can scale out for a more HA deployment
     });
 
+    // Allow traffic to the exit server from anywhere. This allows my Raspberry Pi's to connect
+    // from anywhere on the internet, as long as they have the secret token. It also allows end
+    // users to connect to the exit server from anywhere in order to send traffic back down to my
+    // Raspberry Pi's.
     service.connections.allowFromAnyIpv4(ec2.Port.tcp(8123));
+    service.connections.allowFromAnyIpv4(ec2.Port.tcp(8000));
 
-    // Launch an NLB that will sit in front of the inlet exit server, for HA
+    // Launch an NLB that will sit in front of the inlet exit server, for HA and to get
+    // a stable hostname for sending traffic to.
     const lb = new elbv2.NetworkLoadBalancer(this, 'inlets-lb', { vpc, internetFacing: true });
     const inletsClientListener = lb.addListener('client-listener', { port: 8123 });
 
-    inletsClientListener.addTargets('inlets', {
+    inletsClientListener.addTargets('inlets-private', {
       port: 8123,
-      targets: [service]
+      targets: [service.loadBalancerTarget({
+        containerName: 'inlets',
+        containerPort: 8123,
+      })]
     });
 
     const inletsPublicListener = lb.addListener('public-listener', { port: 80 });
 
-    inletsPublicListener.addTargets('inlets', {
-      port: 8080,
-      targets: [service]
+    inletsPublicListener.addTargets('inlets-public', {
+      port: 80,
+      targets: [service.loadBalancerTarget({
+        containerName: 'inlets',
+        containerPort: 8000,
+      })]
     });
 
-    // Create an Anywhere service to run in the cluster
+    // Create an ECS Anywhere service to run the Inlets clients in my Raspberry
+    // Pi clusters. This is a daemon service that runs one client per Raspberry Pi.
     var clientDefinition = new ecs.ExternalTaskDefinition(this, 'client-inlets');
 
-    clientDefinition.addContainer('inlets', {
+    var clientContainer = clientDefinition.addContainer('inlets', {
       cpu: 256,
       memoryLimitMiB: 256,
       image: ecs.ContainerImage.fromRegistry('ghcr.io/inlets/inlets-pro:0.9.0-rc2'),
       logging: new ecs.AwsLogDriver({ streamPrefix: 'inlets-client' }),
       command: [
-        "http", "client", "--url=ws://" + lb.loadBalancerDnsName + ":8123", "--token-env=INLETS_TOKEN", "--upstream", "localhost:8080",
+        "http", "client", "--url=ws://" + lb.loadBalancerDnsName + ":8123", "--token-env=INLETS_TOKEN", "--upstream", "localhost:80",
         "--license-env=LICENSE_KEY"
       ],
       secrets: {
         INLETS_TOKEN: ecs.Secret.fromSecretsManager(inletsToken),
         LICENSE_KEY: ecs.Secret.fromSecretsManager(licenseKey),
       },
+    });
+
+    clientContainer.addUlimits({
+      softLimit: 1024000,
+      hardLimit: 1024000,
+      name: ecs.UlimitName.NOFILE
     });
 
     //-----------------------------------------------------------------------------------------
@@ -116,7 +151,8 @@ export class InletsStack extends cdk.Stack {
     cfnDaemon.desiredCount = undefined;
     //--------------------------------------------------------------------------------------------------
 
-    // Define the external service that we will expose via Inlets
+    // Now define the external service that we will expose via Inlets
+    // This service will run on the Raspberry Pi's in my home cluster.
     var appDefinition = new ecs.ExternalTaskDefinition(this, 'demo-app-definition');
 
     //-PATCH-------------------------------------------------------------------------------------------
@@ -160,7 +196,7 @@ export class InletsStack extends cdk.Stack {
 
     //------------------------------------------------------------------------------------------------
 
-    appDefinition.addContainer('app', {
+    var appContainer = appDefinition.addContainer('app', {
       cpu: 2048,
       memoryLimitMiB: 2048,
       image: image,
@@ -168,16 +204,22 @@ export class InletsStack extends cdk.Stack {
       command: ['node', 'index.js'],
       portMappings: [
         {
-          hostPort: 8080,
+          hostPort: 80,
           containerPort: 3000
         }
       ]
     });
 
-    var appService = new ecs.ExternalService(this, 'demo-app-service', {
+    appContainer.addUlimits({
+      softLimit: 1024000,
+      hardLimit: 1024000,
+      name: ecs.UlimitName.NOFILE
+    });
+
+    new ecs.ExternalService(this, 'demo-app-service', {
       cluster,
       taskDefinition: appDefinition,
-      desiredCount: 1,
+      desiredCount: 4,
     });
   }
 }
